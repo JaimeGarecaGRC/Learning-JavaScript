6e228d8e28823b55e91f3dc01cd4b887ccc10906
JaimeGarecaGRC <jaimegarecagrc@gmail.com>
2021-11-20 14:48:44 -0400

    #

---
+++ codes/hello world/script.js
@@ -0,0 +1,10 @@
+
+//alert nos mostrara un cuadro en la parte superior de la pagina que nos mostrara el string hello world
+alert("hello world");
+
+//creo dos variables a y b con sus respectivos valores
+let a = 10;
+let b = 50;
+
+//usamos una condicional if para verificar si la suba da a + b y si es asi que nos muestre por la consola del navegador ese string concatenado
+if(a + b === 60) console.log(`El resultado de la suma es ${a + b}`);
