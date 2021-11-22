08ce553496d66bf48c07e340e7bb916a3be2a51e
JaimeGarecaGRC <jaimegarecagrc@gmail.com>
2021-11-21 21:06:18 -0400

    comparadores basicos

---
+++ codes/5 operatoradores basicos/script.js
@@ -0,0 +1,26 @@
+
+//Contamos con operadores matematicos como:
+
+let a = 50;
+const b = 10;
+
+console.log(a + b); // + es el operador de suma
+console.log(a - b); // - es el operador de resta
+console.log(a * b); // * es el operador de multiplicacion
+console.log(a / b); // / es el operador de divicion
+console.log(a ** b); // ** es el operador de elevado a, se lee como a elevado a b
+
+//Contamos con los operadores de asignacion
+
+let c = 40; // = es el operador de asignacion
+c =+ 10; // =+ es el operador de suma y asigna
+c =- 20; // =- es el operador de resta y asigna
+c++; // ++ es el operador para que sume uno a c
+c--; // -- es el operador para que reste uno a c
+ 
+//Contamos con los operadores de comparacion (Estos devuelven un true o false)
+
+console.log(a > b); //  >  es equivalente a decir si a es mayor que b devuelve true, sino false.
+console.log(a >= b); // >= es equivalente a mayor o igual
+console.log(a < b); //  <  es equivalente a menor
+console.log(a <= b); // <= es equivalente a menor o igual
